84161a3edf494bd13fb5eee6ee2b8b001e36ce87
Thomas Bivins <bivinsthomas@gmail.com>
2021-08-07 13:38:56 -0400

    Add files via upload

---
+++ eightball.py
@@ -0,0 +1,26 @@
+import tweepy
+
+all_keys = open('keys.txt', 'r').read().splitlines()
+api_key = all_keys[0]
+api_key_secret = all_keys[1]
+access_token = all_keys[2]
+access_token_secret = all_keys[3]
+
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+"""
+def read_last_seen(FILE_NAME) :
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+"""
